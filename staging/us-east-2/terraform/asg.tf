7a75513d60dd45e7e415d2fc2c423073c3bc2520
Patrick Gilbert <pgcloudsec@gmail.com>
2024-09-20 07:30:00 -0400

    Updating infrasec infrastructure Phase 2 - CS2-168

---
+++ staging/us-east-2/terraform/asg.tf
@@ -0,0 +1,31 @@
+resource "aws_launch_template" "staging-lt" {
+  name = "${var.staging-env}-lt"
+  image_id = "ami-085f9c64a9b75eed5"
+  instance_type = "t2.micro"
+  key_name = "cs2-use2-main"
+  instance_initiated_shutdown_behavior = "terminate"
+  vpc_security_group_ids = [aws_security_group.private_sg.id]
+  
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "${var.staging-env}-lt"
+    }
+  }
+
+  user_data = filebase64("web.sh")
+}
+
+  resource "aws_autoscaling_group" "staging-asg" {
+  name = "${var.staging-env}-asg"
+  vpc_zone_identifier = [aws_subnet.staging_private_subnet_1.id, aws_subnet.staging_private_subnet_2.id]
+  desired_capacity   = 2
+  max_size           = 2
+  min_size           = 1
+
+  launch_template {
+    id      = aws_launch_template.staging-lt.id
+    version = "$Latest"
+  }
+}
--- staging/us-east-2/terraform/dns.tf
+++ staging/us-east-2/terraform/dns.tf
@@ -3,5 +3,21 @@
 #   name    = "www.${data.aws_route53_zone.pgcloudapps.name}"
 #   type    = "A"
 #   ttl     = "300"
-#   records = ["10.0.0.1"]
+#   records = ["10.35.0.1"]
 # }
+
+resource "aws_route53_record" "www_stage" {
+  zone_id = data.aws_route53_zone.pgcloudapps.zone_id
+  name    = "www.${var.staging-env}.pgcloudapps.com"
+  type    = "A"
+  ttl     = "300"
+  records = ["10.35.0.1"]
+}
+
+resource "aws_route53_record" "stage" {
+  zone_id = data.aws_route53_zone.pgcloudapps.zone_id
+  name    = "${var.staging-env}.pgcloudapps.com"
+  type    = "A"
+  ttl     = "300"
+  records = ["10.35.0.2"]
+}
